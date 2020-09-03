b742d59990a5e108b9328b1c12da851cb08dfcc0
an <anna.makarova@saritasa.com>
2020-09-03 20:31:28 +0500

    fix wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for words_learning project.
+WSGI config for words-learning project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'words_learning.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'words-learning.settings')
 
 application = get_wsgi_application()
